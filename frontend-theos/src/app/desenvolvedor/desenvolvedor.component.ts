85ec18835b5789e5285f0e75b9a488e871723dd2
Jonatas de Assis Silva <jonatasassis@MacBook-Pro-de-Jonatas.local>
2020-05-14 20:44:41 -0300

    Iniciando o projeto

---
+++ frontend-theos/src/app/desenvolvedor/desenvolvedor.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-desenvolvedor',
+  templateUrl: './desenvolvedor.component.html',
+  styleUrls: ['./desenvolvedor.component.css']
+})
+export class DesenvolvedorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
