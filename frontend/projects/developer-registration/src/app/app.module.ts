f39fb82f82b0dc4a4c6c185e3e04453726ec3df0
Peterson Salme <petersonsalme@gmail.com>
2020-10-24 11:37:58 -0300

    creating navbar

--- frontend/projects/developer-registration/src/app/app.module.ts
+++ frontend/projects/developer-registration/src/app/app.module.ts
@@ -8,11 +8,16 @@ import { DevCardComponent } from './dev-card/dev-card.component';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { NavbarComponent } from './navbar/navbar.component';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
   declarations: [
     AppComponent,
     DevCardComponent,
+    NavbarComponent,
   ],
   imports: [
     BrowserModule,
@@ -21,8 +26,12 @@ import { MatIconModule } from '@angular/material/icon';
     MatButtonModule,
     MatCardModule,
     MatIconModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatListModule,
   ],
-  providers: [],
+  providers: [
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule { }
---
+++ frontend/projects/developer-registration/src/app/navbar/navbar.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.scss']
+})
+export class NavbarComponent implements OnInit {
+  isOpened = false;
+  isHandset$: Observable<boolean> = this.breakpointObserver
+    .observe(Breakpoints.Handset)
+    .pipe(
+      map(result => result.matches)
+    );
+
+  constructor(private breakpointObserver: BreakpointObserver) {}
+
+  ngOnInit(): void {
+  }
+
+  toggleMenu(): void {
+    this.isOpened = !this.isOpened; 
+  }
+
+}
