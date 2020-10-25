806d99f880d913d624add3494a0693ead0fad614
Peterson Salme <petersonsalme@gmail.com>
2020-10-25 14:10:03 -0300

    turn avatarURL optional

--- backend/src/entity/dev.js
+++ backend/src/entity/dev.js
@@ -2,13 +2,13 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const DevSchema = new Schema({
-    github: {type: String},
-    avatarURL: {type: String, required: true},
-    name: {type: String, required: true, max: 100},
-    email: {type: String, required: true, max: 50},
-    city: {type: String, required: true, max: 100},
-    graduation: {type: String, required: true, max: 100},
-    techStack: {type: String, required: true},
+    github:     { type: String },
+    avatarURL:  { type: String },
+    name:       { type: String, required: true, max: 100 },
+    email:      { type: String, required: true, max: 50 },
+    city:       { type: String, required: true, max: 100 },
+    graduation: { type: String, required: true, max: 100 },
+    techStack:  { type: String, required: true },
 });
 
 module.exports = mongoose.model('Devs', DevSchema);
