3ffd8e30682debaeb70fde145f65cf3fac7d4b3f
Peterson Salme <petersonsalme@gmail.com>
2020-10-25 11:02:10 -0300

    adding github field

--- backend/src/entity/dev.js
+++ backend/src/entity/dev.js
@@ -2,12 +2,13 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const DevSchema = new Schema({
+    github: {type: String, required: true},
     avatarURL: {type: String, required: true},
     name: {type: String, required: true, max: 100},
     email: {type: String, required: true, max: 50},
     city: {type: String, required: true, max: 100},
     gratuation: {type: String, required: true, max: 100},
-    techStack: {type: String, required: true, max: 500},
+    techStack: {type: String, required: true},
 });
 
 module.exports = mongoose.model('Devs', DevSchema);
