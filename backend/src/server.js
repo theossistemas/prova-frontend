4de0a3be2b4fa00ded110e83f7292f5d2acddf1b
Peterson Salme <petersonsalme@gmail.com>
2020-10-25 00:51:06 -0300

    adding trycatch

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,15 +4,18 @@ const bodyParser = require('body-parser');
 const mongodb = require('./database/mongoose');
 const route = require('./routes/index')
 
-const app = express();
-const corsConfig = cors({ origin: ['http://localhost:4000', 'http://localhost:4200'] }); 
-const db = mongodb.connect();
-
-app.use(bodyParser.json());
-app.use(corsConfig);
-app.use(route());
-
-
-app.listen(process.env.PORT || 8000, () => {
-    console.log('Server..: OK');
-});
+try {
+    const app = express();
+    const corsConfig = cors({ origin: ['http://localhost:4000', 'http://localhost:4200'] }); 
+    const db = mongodb.connect();
+    
+    app.use(bodyParser.json());
+    app.use(corsConfig);
+    app.use(route());
+    
+    app.listen(process.env.PORT || 8000, () => {
+        console.log('Server..: OK');
+    });
+} catch (error) {
+    console.error('Failed to start: \n', error.message)    
+}
