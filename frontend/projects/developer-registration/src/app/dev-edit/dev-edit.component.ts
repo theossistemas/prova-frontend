9d31d06b2ee0d6767c8655f6f373cf6df11df879
Peterson Salme <petersonsalme@gmail.com>
2020-10-25 12:59:11 -0300

    implementing reactive form

--- frontend/projects/developer-registration/src/app/dev-edit/dev-edit.component.ts
+++ frontend/projects/developer-registration/src/app/dev-edit/dev-edit.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, FormBuilder } from '@angular/forms';
 import { MatFormFieldControl } from '@angular/material/form-field';
+import { ActivatedRoute, Router } from '@angular/router';
+import { DevInfo } from '../../entities/dev-info';
+import { DevService } from '../../services/dev.service';
 
 @Component({
   selector: 'app-dev-edit',
@@ -8,25 +11,41 @@ import { MatFormFieldControl } from '@angular/material/form-field';
   styleUrls: ['./dev-edit.component.scss']
 })
 export class DevEditComponent implements OnInit {
+  id: string;
+  dev: DevInfo = new DevInfo();
+  editDevForm: FormGroup = this.dev.createForm(this.formBuilder);
 
-  editDevForm: FormGroup = this.formBuilder.group({
-    avatarURL: [''],
-    name: [''],
-    email: [''],
-    city: [''],
-    graduation: [''],
-    techStack: [''],
-  });
-
-  nameControl: MatFormFieldControl<any>;
-
-  constructor(private formBuilder: FormBuilder) { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router,
+    private route: ActivatedRoute,
+    private devService: DevService
+  ) { }
 
   ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      const { id } = params;
+      this.devService.getOne(id).subscribe(
+        dev => {
+          this.id = id;
+          this.dev = dev;
+          this.editDevForm.patchValue(dev);
+        },
+        err => console.error(err)
+      );
+    });
   }
 
   onSubmit(): void {
-    console.log(this.editDevForm.value);
+    console.log('onSubmit(', this.id, ')');
+    this.dev = this.editDevForm.value as DevInfo;
+
+    if (this.id) {
+      this.devService.put(this.id, this.dev).subscribe(
+        () => this.router.navigateByUrl('/devs'),
+        (err) => console.error(err)
+      );
+    }
   }
 
 }
