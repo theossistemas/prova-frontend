213df4fd05e6315795349bf180a014b135b38ee8
Peterson Salme <petersonsalme@gmail.com>
2020-10-25 12:55:22 -0300

    adding github attr and creating method for build form groups

--- frontend/projects/developer-registration/src/entities/dev-info.ts
+++ frontend/projects/developer-registration/src/entities/dev-info.ts
@@ -1,9 +1,25 @@
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
 export class DevInfo {
     id: string;
+    github: string;
     avatarURL: string;
     name: string;
     email: string;
     city: string;
-    gratuation: string;
+    graduation: string;
     techStack: string;
+
+    public createForm(formBuilder: FormBuilder): FormGroup {
+        return formBuilder.group({
+          id: this.id,
+          github: this.github,
+          avatarURL: this.avatarURL,
+          name: [this.name, [Validators.required]],
+          email: [this.email, [Validators.required, Validators.email]],
+          city: [this.city, [Validators.required]],
+          graduation: [this.graduation, [Validators.required]],
+          techStack: [this.techStack, [Validators.required]]
+        });
+      }
 }
