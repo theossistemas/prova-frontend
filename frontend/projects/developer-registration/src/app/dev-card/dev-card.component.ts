2ab85a1fdc5cf1c1aaa94a30140ec9cb4a9f5965
Peterson Salme <petersonsalme@gmail.com>
2020-10-25 12:57:46 -0300

    general implementations

--- frontend/projects/developer-registration/src/app/dev-card/dev-card.component.ts
+++ frontend/projects/developer-registration/src/app/dev-card/dev-card.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { DevInfo } from 'projects/developer-registration/src/entities/dev-info';
+import { DevService } from '../../services/dev.service';
 
 @Component({
   selector: 'app-dev-card',
@@ -8,11 +10,27 @@ import { DevInfo } from 'projects/developer-registration/src/entities/dev-info';
 })
 export class DevCardComponent implements OnInit {
   @Input() dev: DevInfo;
+  @Input() devList: DevInfo[];
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private devService: DevService
+  ) { }
 
   ngOnInit(): void {
-    console.log(this.dev.name);
   }
 
+  editDev(): void {
+    this.router.navigate(['/edit-dev/' + this.dev.id]);
+  }
+
+  deleteDev(): void {
+    this.devService.delete(this.dev.id).subscribe(
+      () => {
+        const index = this.devList.findIndex(d => d.id === this.dev.id);
+        this.devList.splice(index, 1);
+      },
+      err => console.error(err)
+    );
+  }
 }
