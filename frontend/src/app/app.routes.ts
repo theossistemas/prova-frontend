2e5c2116a273168ba3779100d8ba180e1e41be01
Fernando Ogata <fernandokogata@gmail.com>
2024-11-04 23:02:15 -0300

    Frontend - Criados arquivos de Home, Login, Register, configuradas rotas e guarda de rotas.

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    title: 'HOME',
+    loadComponent: () => import('./pages/home/home.component').then((m) => m.HomeComponent)
+  },
+  {
+    path: 'login',
+    title: 'LOGIN',
+    loadComponent: () => import('./pages/login/login.component').then((m) => m.LoginComponent)
+  },
+  {
+    path: 'register',
+    title: 'REGISTER',
+    loadComponent: () => import('./pages/register/register.component').then((m) => m.RegisterComponent)
+  }
+];
